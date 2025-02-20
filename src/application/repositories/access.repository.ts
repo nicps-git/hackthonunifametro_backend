ad95101c2f7afef8c4a1b76461dc4d4a27e266ad
Italo Moraes Almeida <italomoraesalmeida@hotmail.com>
2025-02-20 20:48:04 -0300

    :rocket:(feature) Configurado recuperação de senha do usuário

--- src/application/repositories/access.repository.ts
+++ src/application/repositories/access.repository.ts
@@ -8,4 +8,12 @@ export interface IAccessRepository {
 
 export abstract class AccessRepositories {
   abstract login(access: TAccessSchema): Promise<IAccessRepository | undefined>;
+
+  abstract requestResetPassword(email: string): Promise<boolean>;
+
+  abstract saveCodeResetPassword(email: string, code: string): Promise<boolean>;
+
+  abstract resetPassword(password: string, code: string): Promise<boolean>;
+
+  abstract disableCodeResetPassword(code: string): Promise<void>;
 }
