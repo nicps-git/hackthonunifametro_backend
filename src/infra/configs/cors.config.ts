79296cc6947068902c63f106f4f7a026a748e32b
Italo Moraes Almeida <italomoraesalmeida@hotmail.com>
2025-03-07 10:52:22 -0300

    :pencil2:(chore) Liberação total do CORS provisorio

--- src/infra/configs/cors.config.ts
+++ src/infra/configs/cors.config.ts
@@ -2,7 +2,8 @@ import { INestApplication } from '@nestjs/common';
 
 export const corsConfig = (app: INestApplication<any>) => {
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
+    // origin: 'http://localhost:3000',
     methods: 'GET,PUT,PATCH,POST,DELETE',
     allowedHeaders:
       'Content-type, Accept, Access-Control-Allow-Origin, Access-Control-Allow-Headers',
